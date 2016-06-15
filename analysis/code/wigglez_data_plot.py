af29c3eb2be7776e6e307cdcc67b531314a19823
Samuel Hinton <samuelreay@gmail.com>
2016-06-14 23:39:38 -0700

    Adding big plot. Still needs work

--- analysis/code/wigglez_data_plot.py
+++ analysis/code/wigglez_data_plot.py
@@ -6,20 +6,117 @@ precon_matrx_z0_file = "../data/wizcola/multipoles/cov/WizCOLA_Cij_xi0xi2_combin
 precon_matrx_z1_file = "../data/wizcola/multipoles/cov/WizCOLA_Cij_xi0xi2_combined_z0pt4_0pt8.dat"
 precon_matrx_z2_file = "../data/wizcola/multipoles/cov/WizCOLA_Cij_xi0xi2_combined_z0pt6_1pt0.dat"
 
-recon_matrx_z0_file = "../data/wizcolarecon/WiZCOLA_xiL_postrecon_allmocks"
-recon_matrx_z1_file = "../data/wizcolarecon/multipoles/cov/WizCOLA_Cij_xi0xi2_combined_z0pt4_0pt8.dat"
-recon_matrx_z2_file = "../data/wizcolarecon/multipoles/cov/WizCOLA_Cij_xi0xi2_combined_z0pt6_1pt0.dat"
+recon_matrx_z0_file = "../data/wizcola/multipoles/cov/WizCOLA_Cij_xi0xi2_combined_z0pt2_0pt6.dat"
+recon_matrx_z1_file = "../data/wizcola/multipoles/cov/WizCOLA_Cij_xi0xi2_combined_z0pt4_0pt8.dat"
+recon_matrx_z2_file = "../data/wizcola/multipoles/cov/WizCOLA_Cij_xi0xi2_combined_z0pt6_1pt0.dat"
+
+precon_z0_file = "../data/wiggleznew/multipoles/WiggleZ_xi0xi2_all6regions_z0pt2_0pt6.dat"
+precon_z1_file = "../data/wiggleznew/multipoles/WiggleZ_xi0xi2_all6regions_z0pt4_0pt8.dat"
+precon_z2_file = "../data/wiggleznew/multipoles/WiggleZ_xi0xi2_all6regions_z0pt6_1pt0.dat"
+
+recon_z0_file = "../data/wigglezrecon/WiggleZ_xiLxiT_z26.ascii"
+recon_z1_file = "../data/wigglezrecon/WiggleZ_xiLxiT_z48.ascii"
+recon_z2_file = "../data/wigglezrecon/WiggleZ_xiLxiT_z60.ascii"
 
 covs = []
 rcovs = []
 for file in [precon_matrx_z0_file, precon_matrx_z1_file, precon_matrx_z2_file]:
     data = np.loadtxt(file)[:, 3].reshape(60, 60)
-    print(data.shape)
-    covs.append(data)
+    #print(data.shape)
+    R = np.zeros(data.shape)
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            R[i,j] = data[i, j] / np.sqrt(data[i,i] * data[j,j])
+    covs.append(R)
     
 for file in [recon_matrx_z0_file, recon_matrx_z1_file, recon_matrx_z2_file]:
+    data = np.loadtxt(file)[:, 3].reshape(60, 60)
+    #print(data.shape)
+    rcovs.append(data)
+      
+#for file in [recon_matrx_z0_file, recon_matrx_z1_file, recon_matrx_z2_file]:
 
-fig, axes = plt.subplots(4, 3, figsize=(10,10))
-axes[1,0].imshow(covs[0], cmap="viridis")
-axes[1,1].imshow(covs[1], cmap="viridis")
-axes[1,2].imshow(covs[2], cmap="viridis")
+ss = []
+vals_m = []
+vals_me = []
+vals_q = []
+vals_qe = []
+for file in [precon_z0_file, precon_z1_file, precon_z2_file]:
+    data = np.loadtxt(file)
+    ss.append(data[:,2])
+    vals_m.append(data[:,3])
+    vals_me.append(data[:,4])
+    vals_q.append(data[:,5])
+    vals_qe.append(data[:,6])
+    
+ssr = []
+vals_t = []
+vals_te = []
+vals_l = []
+vals_le = []
+for file in [recon_z0_file, recon_z1_file, recon_z2_file]:
+    data = np.loadtxt(file)
+    ssr.append(data[:,0])
+    vals_t.append(data[:,1])
+    vals_te.append(data[:,2])
+    vals_l.append(data[:,4])
+    vals_le.append(data[:,5])
+    
+f =13
+fig, axes = plt.subplots(4, 3, figsize=(12,14))
+
+axes[0,0].set_title("$0.2 < z < 0.6$")
+axes[0,1].set_title("$0.4 < z < 0.8$")
+axes[0,2].set_title("$0.6 < z < 1.0$")
+
+for i,(s,v,e) in enumerate(zip(ss, vals_m, vals_me)):
+    axes[0,i].errorbar(s, s*s*v, yerr=s*s*e, fmt='o', color='b', label="Monopole")
+    axes[0,i].set_xlabel(r"$s\ \  \left[{\rm Mpc}\, h^{-1}\right]$",fontsize=f)
+    axes[0,i].set_ylabel(r"$s^2 \xi(s) \ \left[ {\rm Mpc}^{2} \, h^{-2}  \right]$",fontsize=f)
+
+    
+for i,(s,v,e) in enumerate(zip(ss, vals_q, vals_qe)):
+    axes[0,i].errorbar(s, s*s*v, yerr=s*s*e, fmt='o', color='r', label="Quadrupole")
+    if i == 0:
+        axes[0,i].legend(frameon=False, loc=2)
+for i, cov in enumerate(covs):
+    axes[1,i].imshow(cov, cmap="viridis",interpolation='none')
+    axes[1,i].axvline(30, color='white', alpha=0.3)
+    axes[1,i].axhline(30, color='white', alpha=0.3)
+    axes[1,i].set_xticks([0,15,30,45,59])
+    axes[1,i].set_xticklabels([0,100,200,100,200])
+    axes[1,i].set_yticks([0,15,30,45,59])
+    axes[1,i].set_yticklabels([0,100,200,100,200])
+    axes[1,i].set_xlabel(r"$s\ \  \left[{\rm Mpc}\, h^{-1}\right]$",fontsize=f)
+    axes[1,i].set_ylabel(r"$s\ \  \left[{\rm Mpc}\, h^{-1}\right]$",fontsize=f)
+    axes[1,i].text(45, 5, r"$\xi_0 \times \xi_2$", color="white", fontsize=f)
+    axes[1,i].text(25, 5, r"$\xi_0$", color="white", fontsize=f)
+    axes[1,i].text(55, 35, r"$\xi_2$", color="white", fontsize=f)
+
+for i,(s,v,e) in enumerate(zip(ssr, vals_t, vals_te)):
+    axes[2,i].errorbar(s, s*s*v, yerr=s*s*e, fmt='o', color='b', label="Transverse")
+    axes[2,i].set_xlabel(r"$s\ \  \left[{\rm Mpc}\, h^{-1}\right]$",fontsize=f)
+    axes[2,i].set_ylabel(r"$s^2 \xi(s) \ \left[ {\rm Mpc}^{2} \, h^{-2}  \right]$",fontsize=f)
+    
+for i,(s,v,e) in enumerate(zip(ssr, vals_l, vals_le)):
+    axes[2,i].errorbar(s, s*s*v, yerr=s*s*e, fmt='o', color='r', label="Line of sight")
+    if i == 0:
+        axes[0,i].legend(frameon=False, loc=3)
+for i, cov in enumerate(rcovs):
+    axes[3,i].imshow(cov, cmap="viridis",interpolation='none')
+    axes[3,i].axvline(30, color='white', alpha=0.3)
+    axes[3,i].axhline(30, color='white', alpha=0.3)
+    axes[3,i].set_xticks([0,15,30,45,59])
+    axes[3,i].set_xticklabels([0,100,200,100,200])
+    axes[3,i].set_yticks([0,15,30,45,59])
+    axes[3,i].set_yticklabels([0,100,200,100,200])
+    axes[3,i].set_xlabel(r"$s\ \  \left[{\rm Mpc}\, h^{-1}\right]$",fontsize=f)
+    axes[3,i].set_ylabel(r"$s\ \  \left[{\rm Mpc}\, h^{-1}\right]$",fontsize=f)
+    axes[3,i].text(45, 5, r"$\xi_\parallel \times \xi_\perp$", color="white", fontsize=f)
+    axes[3,i].text(25, 5, r"$\xi_\parallel$", color="white", fontsize=f)
+    axes[3,i].text(55, 35, r"$\xi_\perp$", color="white", fontsize=f)
+    
+plt.tight_layout()
+fig.savefig("dataplot.pdf", bbox_inches="tight", dpi=300)
+fig.savefig("dataplot.png", bbox_inches="tight", dpi=300)
+    
---
+++ analysis/data/bWigMpBin/get_da_hz.py
@@ -0,0 +1,100 @@
+import numpy as np
+import os
+from chain import ChainConsumer
+from scipy.integrate import simps, cumtrapz
+from scipy.interpolate import interp1d
+
+def get_r_s(oms):
+    # Fiducial
+    h = 0.705
+    ob = 0.0456
+    ol = 0.727
+    og = 2.469e-5 / (h * h)
+    orr = og * (1 + (7.0/8.0)*np.power(4.0/11.0, 4.0/3.0)*3.04)
+    
+    c = 3e5
+    z = 1020
+    ae = 1.0 / (1.0 + z)
+    a = np.linspace(ae/100000, ae, 100)
+    
+    rs = []
+    for om in oms:
+        ez = np.sqrt(om / (a*a*a) + ol + orr/(a*a*a*a))
+        hs = h * ez
+        fac = 1/np.sqrt(3 * (1 + (3 * ob / (4 * og))*a))
+        integrand = (c/100) * fac / (a*a*hs)
+        integral = simps(integrand, x=a)
+        rs.append(integral)
+    return rs
+    
+def load_directory(directory):
+    
+    num_burnin = 20000
+    res = None
+    for f in os.listdir(directory):
+        filename = directory + os.sep + f
+        if not os.path.isfile(filename):
+            continue
+        matrix = np.load(filename)[num_burnin:, [3,-2,-1]]
+        if res is None:
+            res = matrix
+        else:
+            res = np.vstack((res, matrix))
+    return res
+    
+def convert_directory(directory, z):
+    data = load_directory(directory)
+    omch2 = data[:,0]
+    alpha = data[:,1]
+    epsilon = data[:,2]
+    
+    # Fiducial
+    h = 0.705
+    om = 0.273
+    ob = 0.0456
+    ol = 0.727
+    H = 100
+    c = 3e5
+    #omall = np.linspace(0.05, 0.3, 1000)
+    #rsall = get_r_s(omall)
+    
+    #oms = omch2/h/h + ob
+    #rss = interp1d(omall, rsall)(oms)
+    
+    zs = np.linspace(0,z,1000)
+    ez = np.sqrt(om * (1 + zs)**3 + ol)
+    
+    iez = 1 / ez
+    
+    chi = simps(iez, zs)
+    da = (c/(H * h)) * chi / (1 + z)
+    hs = h * H * ez[-1]
+    
+    rs_fid = get_r_s([0.273])[0]
+    
+    daval = (alpha/(1+epsilon)) * da #/ rs_fid
+    
+    hrc = hs / (alpha * (1 + epsilon) * (1 + epsilon)) #* rs_fid / (alpha * (1 + epsilon) * (1 + epsilon)) / c
+    res = np.vstack((omch2, daval, hrc)).T
+    return res
+    
+    
+    
+if False:
+    consumer = ChainConsumer()
+    consumer.configure_contour(sigmas=[0,1,2])
+    consumer.add_chain(load_directory("../bWigMpBin/bWigMpBin_z0"), parameters=[r"$\Omega_c h^2$", r"$\alpha$", r"$\epsilon$"], name="$0.2<z<0.6$")
+    consumer.add_chain(load_directory("../bWigMpBin/bWigMpBin_z1"), parameters=[r"$\Omega_c h^2$", r"$\alpha$", r"$\epsilon$"], name="$0.4<z<0.8$")
+    consumer.add_chain(load_directory("../bWigMpBin/bWigMpBin_z2"), parameters=[r"$\Omega_c h^2$", r"$\alpha$", r"$\epsilon$"], name="$0.6<z<1.0$")
+    consumer.plot(figsize="column", filename="wigglez_multipole_alphaepsilon.pdf")
+    #print(consumer.get_latex_table())
+
+if True:
+    c = ChainConsumer()
+    c.add_chain(convert_directory("../bWigMpBin/bWigMpBin_z0", 0.44), parameters=[r"$\Omega_c h^2$", r"$D_A(z)$", r"$H(z)$"], name="$0.2<z<0.6$")
+    c.add_chain(convert_directory("../bWigMpBin/bWigMpBin_z1", 0.60), parameters=[r"$\Omega_c h^2$", r"$D_A(z)$", r"$H(z)$"], name="$0.4<z<0.8$")
+    c.add_chain(convert_directory("../bWigMpBin/bWigMpBin_z2", 0.73), parameters=[r"$\Omega_c h^2$", r"$D_A(z)$", r"$H(z)$"], name="$0.6<z<1.0$")
+    c.configure_contour(sigmas=[0,1,2])
+    c.configure_general(bins=0.7)    
+    c.plot(figsize="column", filename="wigglez_multipole_dah.pdf")
+    print(c.get_latex_table())
